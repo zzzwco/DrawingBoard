c2f14bac42f591168bb2554b7ec58e4e3efec6bf
zzzwco <zzzwco@outlook.com>
2021-09-15 16:18:23 +0800

    init

---
+++ Shared/DrawingBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingBoardApp.swift
+//  Shared
+//
+//  Created by Bruce on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct DrawingBoardApp: App {
+  var body: some Scene {
+    WindowGroup {
+      DrawingView()
+    }
+  }
+}
